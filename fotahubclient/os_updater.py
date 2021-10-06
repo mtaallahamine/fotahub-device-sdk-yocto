9f75dd43a6000b4c92ddd15d7edf26ecd3fa4b8a
FotaHub <>
2021-10-06 11:17:21 +0200

    FotaHub Device SDK for Yocto 1.0.0-10

--- fotahubclient/os_updater.py
+++ fotahubclient/os_updater.py
@@ -13,9 +13,9 @@ OSTREE_REMOTE_URL = 'https://ostree.fotahub.com'
 OSTREE_REMOTE_GPG_VERIFY = False
 OSTREE_PULL_DEPTH = 1
 
-UBOOT_ACTIVATE_OS_UPDATE_FLAG = 'os_update_activated'
-UBOOT_CONFIRM_OS_UPDATE_FLAG = 'os_update_confirmed'
-UBOOT_REVERT_OS_UPDATE_FLAG = 'os_update_reverted'
+UBOOT_FLAG_ACTIVATING_OS_UPDATE = 'acivating_os_update'
+UBOOT_FLAG_REVERTING_OS_UPDATE = 'reverting_os_update'
+UBOOT_VAR_OS_UPDATE_BOOT_FAILURE_CREDIT = 'os_update_boot_failure_credit'
 
 class OSUpdater(OSTreeClient, UBootOperator):
 
@@ -64,18 +64,20 @@ class OSUpdater(OSTreeClient, UBootOperator):
         except GLib.Error as err:
             raise OSTreeError("Failed to stage OS update with revision '{}'".format(revision)) from err
 
-    def activate_os_update(self):
+    def activate_os_update(self, max_boot_failure_count):
         self.logger.info("Activating staged OS update")
-        self.set_uboot_env_var(UBOOT_ACTIVATE_OS_UPDATE_FLAG, '1')
+        self.set_uboot_env_var(UBOOT_FLAG_ACTIVATING_OS_UPDATE, '1')
+        self.set_uboot_env_var(UBOOT_VAR_OS_UPDATE_BOOT_FAILURE_CREDIT, str(max_boot_failure_count))
         self.reboot()
 
     def is_activating_os_update(self):
         self.logger.info("Checking if an OS update is about to be activated")
-        return self.isset_uboot_env_var(UBOOT_ACTIVATE_OS_UPDATE_FLAG)
+        return self.isset_uboot_env_var(UBOOT_FLAG_ACTIVATING_OS_UPDATE)
 
     def confirm_os_update(self):
         self.logger.info("Confirming activated OS update")
-        self.set_uboot_env_var(UBOOT_CONFIRM_OS_UPDATE_FLAG, '1')
+        self.set_uboot_env_var(UBOOT_FLAG_ACTIVATING_OS_UPDATE)
+        self.set_uboot_env_var(UBOOT_VAR_OS_UPDATE_BOOT_FAILURE_CREDIT)
 
     def has_os_update_reverted(self, revision):
         self.logger.info("Checking if OS update has reverted to previously installed revision")
@@ -89,11 +91,12 @@ class OSUpdater(OSTreeClient, UBootOperator):
                     "Cleaning up reverted OS update with revision '{}'".format(pending.get_csum()))
                     
                 # 0 is the index of the pending deployment
+                # TODO Reimplement this behavior using OSTree API (see https://github.com/ostreedev/ostree/blob/8cb5d920c4b89d17c196f30f2c59fcbd4c762a17/src/ostree/ot-admin-builtin-undeploy.c#L59)
                 subprocess.run(["ostree", "admin", "undeploy", "0"], check=True)
         except subprocess.CalledProcessError as err:
             raise OSTreeError("Failed to undeploy reverted OS update") from err
 
     def revert_os_update(self):
         self.logger.info("Reverting activated OS update")
-        self.set_uboot_env_var(UBOOT_REVERT_OS_UPDATE_FLAG, '1')
+        self.set_uboot_env_var(UBOOT_FLAG_REVERTING_OS_UPDATE, '1')
         self.reboot()
