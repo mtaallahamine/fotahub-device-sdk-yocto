173a2e022e5335acc4c6deb1a636fadcbba32a9e
FotaHub <>
2021-10-21 21:28:53 +0200

    FotaHub Device SDK for Yocto 1.0.0-56

--- fotahubclient/json_encode_decode.py
+++ fotahubclient/json_encode_decode.py
@@ -2,8 +2,11 @@ from enum import Enum
 from json import JSONEncoder, JSONDecoder
 import stringcase
 
-def to_pascalcase_keyed_dict(obj):
-    return { stringcase.pascalcase(k): v for k, v in obj.__dict__.items() }
+def none_to_empty_string_valued_dict(dict):
+    return { k: '' if v is None else v for k, v in dict.items() }
+
+def to_pascalcase_keyed_dict(dict):
+    return { stringcase.pascalcase(k): v for k, v in dict.items() }
 
 def from_pascalcase_keyed_dict(dict, enum_types=[]):
     return { stringcase.snakecase(k): to_enum_literal(v, enum_types) for k, v in dict.items() }
@@ -20,15 +23,15 @@ class PascalCaseJSONEncoder(JSONEncoder):
         if isinstance(obj, Enum):
             return obj.name
         else:
-            return to_pascalcase_keyed_dict(obj)
+            return to_pascalcase_keyed_dict(none_to_empty_string_valued_dict(obj.__dict__))
 
 class PascalCasedObjectArrayJSONDecoder(JSONDecoder):
     
-    def __init__(self, array_type, object_type, enum_types=[]):
+    def __init__(self, array_type, object_type, enum_types=None):
         super().__init__(object_hook=self.object_hook)
         self.array_type = array_type
         self.object_type = object_type
-        self.enum_types = enum_types
+        self.enum_types = enum_types if enum_types is not None else []
 
     def object_hook(self, data):
         if len(data) == 1 and isinstance(list(data.values())[0], list):
